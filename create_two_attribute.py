69bf38131fedbf87a80582578847c8586fd5b2a0
Abdumajid05 <abdumajidabdulazizov513@gmail.com>
2024-05-18 14:32:47 +0500

    new

--- create_two_attribute.py
+++ create_two_attribute.py
@@ -1,3 +1,11 @@
 #Create a "Person" class
 
 #that has a name("name") and a age("age")
+class Person:
+    def __init__(self,name_,age):
+        self.name_ = name_
+        self.age = age
+        
+p1 = Person('Ali',25)
+
+print(f"ismi {p1.name_}, yoshi {p1.age}")
